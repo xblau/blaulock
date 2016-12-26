8c718a7ab2f4c28474a6be71db218d56c796c98a
Daniel Mosquera <daniel@blaudev.es>
2016-12-26 20:37:42 +0100

    Support for custom themes on advanced computers

--- app/BlauLock.Main.lua
+++ app/BlauLock.Main.lua
@@ -41,46 +41,58 @@ end
 
 if( Config['Enabled'] ) then
     local Locked = true
-
+    
+    local theme = {
+        window  = colors.gray,
+        error   = colors.white,
+        success = colors.white,
+        osver   = colors.white,
+        text    = colors.white,
+        backg   = colors.black
+    }
+    
     if( term.isColor() ) then
-        windowColor  = colors.blue
-        errorColor   = colors.red
-        successColor = colors.green
-        osverColor   = colors.yellow
-    else
-        windowColor  = colors.gray
-        errorColor   = colors.white
-        successColor = colors.white
-        osverColor   = colors.white
+        if Config['Theme'] ~= nil and type( Config['Theme'] ) == 'table' then
+            theme = Config['Theme']
+        else
+            theme = {
+                window  = colors.blue,
+                error   = colors.red,
+                success = colors.green,
+                osver   = colors.yellow,
+                text    = colors.white,
+                backg   = colors.black
+            }
+        end
     end
 
-    paintutils.drawFilledBox( 1, 1, 51, 19, colors.black )
-    paintutils.drawFilledBox( 14, 6, 38, 12, windowColor )
+    paintutils.drawFilledBox( 1, 1, 51, 19, theme.backg )
+    paintutils.drawFilledBox( 14, 6, 38, 12, theme.window )
 
-    pprint( '+', 13, 5, colors.white, windowColor )
-    pprint( '+', 13, 13, colors.white, windowColor )
-    pprint( '+', 39, 5, colors.white, windowColor )
-    pprint( '+', 39, 13, colors.white, windowColor )
+    pprint( '+', 13, 5, theme.text, theme.window )
+    pprint( '+', 13, 13, theme.text, theme.window )
+    pprint( '+', 39, 5, theme.text, theme.window )
+    pprint( '+', 39, 13, theme.text, theme.window )
 
-    for i = 1, 25 do pprint( '=', 13 + i, 5, colors.white, windowColor ) end
-    for i = 1, 25 do pprint( '=', 13 + i, 13, colors.white, windowColor ) end
-    for i = 1, 7 do pprint( '|', 13, 5 + i, colors.white, windowColor ) end
-    for i = 1, 7 do pprint( '|', 39, 5 + i, colors.white, windowColor ) end
+    for i = 1, 25 do pprint( '=', 13 + i, 5, theme.text, theme.window ) end
+    for i = 1, 25 do pprint( '=', 13 + i, 13, theme.text, theme.window ) end
+    for i = 1, 7 do pprint( '|', 13, 5 + i, theme.text, theme.window ) end
+    for i = 1, 7 do pprint( '|', 39, 5 + i, theme.text, theme.window ) end
 
-    pprint( '.:: BlauLock ::.', 'centrated', 7, colors.white, windowColor )
-    pprint( 'Pass: ', 16, 10, colors.white, windowColor )
+    pprint( '.:: BlauLock ::.', 'centrated', 7, theme.text, theme.window )
+    pprint( 'Pass: ', 16, 10, theme.text, theme.window )
 
     while Locked do
-        local password = pread( 22, 10, 37, colors.white, windowColor, '*' )
+        local password = pread( 22, 10, 37, theme.text, theme.window, '*' )
 
         if BlauLock.VerifyHash( password, Config['Hash'] ) then
             Locked = false
 
-            paintutils.drawLine( 22, 10, 37, 10, windowColor )
-            pprint( 'Access granted!', 22, 10, successColor, windowColor )
+            paintutils.drawLine( 22, 10, 37, 10, theme.window )
+            pprint( 'Access granted!', 22, 10, theme.success, theme.window )
             sleep( 2 )
 
-            term.setTextColor( osverColor )
+            term.setTextColor( theme.osver )
             term.setBackgroundColor( colors.black )
 
             term.clear()
@@ -90,8 +102,8 @@ if( Config['Enabled'] ) then
 
             term.setTextColor( colors.white )
         else
-            paintutils.drawLine( 22, 10, 37, 10, windowColor )
-            pprint( 'Access denied!', 22, 10, errorColor, windowColor )
+            paintutils.drawLine( 22, 10, 37, 10, theme.window )
+            pprint( 'Access denied!', 22, 10, theme.error, theme.window )
             sleep( 3 )
         end
     end
